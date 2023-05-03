59d6712148a4900471d23dc6738275f63464e4c2
AllofTheWeather <78624099+AllofTheWeather@users.noreply.github.com>
2023-05-03 13:30:33 +0100

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,7 @@
 import { createContext } from "react"
 
+/* variables with global scope are initialised here for convienience */
+
 export const rootPath = "/gh-pages-deployment/"
 export const UserContext = createContext()
 export const defaultStyling = "nav-link"
